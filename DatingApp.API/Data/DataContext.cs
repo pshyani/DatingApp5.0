df95b6c5cf634153322e3a0d68b91e6e0c420376
Pankaj Shyani <pshyani@yahoo.com>
2021-01-19 13:04:09 -0500

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Entities;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
